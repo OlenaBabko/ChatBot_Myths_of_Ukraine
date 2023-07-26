d4fed4fe2203ca35ec9acab7e329b17a71b9ba38
ljolia <elena.babko@gmail.com>
2023-07-26 20:06:10 +0300

    handle message, error

--- main.py
+++ main.py
@@ -38,3 +38,48 @@ async def send_constellation_details(update: Update):
 
 
 
+# IF USER CONTACT A BOT
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
+
+    print(f'User ({update.message.chat.id}) in {message_type}: "{text}"')
+
+    if message_type == 'group':
+        if BOT_USERNAME in text:
+            new_text: str = text.replace(BOT_USERNAME, '').strip()
+            response: str = handle_response(new_text)
+        else:
+            return
+    else:
+        response: str = handle_response(text)
+
+    print('Bot:', response)
+    await update.message.reply_text(response)
+
+
+
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f'Update {update} caused error {context.error}')
+
+
+
+
+if __name__ == '__main__':
+    print('starting bot...')
+    app = Application.builder().token(TOKEN).build()
+
+    # COMMANDS
+    app.add_handler(CommandHandler('start', start_command))
+    app.add_handler(CommandHandler('help', help_command))
+    app.add_handler(CommandHandler('birthday', birthday_command))
+
+    # MESSAGES
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+
+    # ERRORS
+    app.add_error_handler(error)
+
+    # POLLS THE BOT
+    print('polling...')
+    app.run_polling(poll_interval=3)
