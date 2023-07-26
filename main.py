baca31d8ccf2a35453d47cbd8a3c7e2fee198f69
ljolia <elena.babko@gmail.com>
2023-07-26 20:01:52 +0300

    hide token

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from typing import Final
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
-TOKEN: Final = "6559055945:AAFS3KeP3ucCGfiLGdlAhckLTWj8lbcRzOM"
+with open('token.txt', 'r') as f:
+    TOKEN = f.read()
+
 BOT_USERNAME: Final = "@old_molfar_bot"
